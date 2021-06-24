430efb7a8cd27c9ef283c655ee8fbc209b405b4a
Marlon Clay <luciousmc@icloud.com>
2021-06-24 12:38:24 -0700

    basic setup complete

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              '@babel/plugin-transform-react-jsx'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+}
